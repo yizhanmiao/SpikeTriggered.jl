c3bf498ae124c005214b69a627abad12e443bdb2
Yizhan Miao <yizhanmiao@pm.me>
2025-05-18 10:19:32 -0700

    update correlogram.jl documentation and comments

--- src/Stats/correlogram.jl
+++ src/Stats/correlogram.jl
@@ -1,3 +1,6 @@
+export spike_xcorr, spike_xcorr_shifted
+# export cross_correlation, cross_correlation_window, auto_correlation
+# export covariogram
 
 # @doc raw"""
 #     correlogram(target::Vector{T}, reference::Vector{T}; bin_size, window_size, is_auto) where {T} -> (Vector, Vector)
@@ -20,7 +23,7 @@
 # """
 # function correlogram(target::Vector{T}, reference::Vector{T}; bin_size=1e-2, window_size=301, is_auto=false) where {T}
 #     _K_half = div(window_size, 2)
-#     _bin_boundary = (-_K_half:_K_half) .* bin_size  #FIXME: boundary not right
+#     _bin_boundary = (-_K_half:_K_half) .* bin_size  #NOTE: boundary not right
 #     output = zeros(Int64, window_size-1)
 #     for pt in reference
 #         _offset = target .- pt
@@ -45,12 +48,12 @@ any bias with a shift predictor.
 function spike_xcorr(
     target::AbstractVector{T}, reference::AbstractVector{T}, roi::AbstractVector
 ) where {T<:Real}
-    rez = zeros(Int, length(roi)-1)
+    rez = zeros(Int, length(roi) - 1)
     for ref_t in reference
         _my_psth = spike_histogram(target .- ref_t, roi)
         rez .+= _my_psth
     end
-    rez
+    return rez
 end
 
 @doc raw"""
@@ -61,7 +64,7 @@ at specific time delays with respect the spikes of the reference cell.
 Corrected with a shift predictor.
 
 ## Keyword Arguments:
-- shift_t: Δt of each shift, usually shift the spike by trials
+- shift_t::Vector: Δt of each shift, usually shift the spike by trials
 - shift_t_end: the end limit of spike_time, usually the end time of the last trial. [default: maximum(reference)]
 """
 function spike_xcorr_shifted(
@@ -73,17 +76,24 @@ function spike_xcorr_shifted(
 ) where {T<:Real}
     raw = spike_xcorr(target, reference, roi)
     shift_t_end = isnothing(shift_t_end) ? maximum(reference) : shift_t_end
-    shift_predictor_mat = zeros(Int, length(roi)-1, length(shift_t))
+    shift_predictor_mat = zeros(Int, length(roi) - 1, length(shift_t))
     for (idx, shift_it) in enumerate(shift_t)
         _shifted_ref = reference .- shift_it
-        _shifted_ref[_shifted_ref .< 0] .+= .+ shift_t_end
+        _shifted_ref[_shifted_ref .< 0] .+= .+shift_t_end
         shift_predictor_mat[:, idx] = spike_xcorr(target, _shifted_ref, roi)
     end
-    raw .- mean(shift_predictor_mat, dims=2)[:], shift_predictor_mat
+    return raw .- mean(shift_predictor_mat; dims=2)[:], shift_predictor_mat
 end
 
 #TODO: crosscorrelogram from rasters, spike_xcorr_shifted(target_raster, reference_raster, roi::AbstractVector)
 
+@doc raw"""
+    cross_correlation(f::AbstractArray{T}, g::AbstractArray{T}, dims=1) where {T <: Real}
+
+Cross correlation between two waveforms/histograms `f` and `g` along the specified dimension `dims`,
+calculated with FFT.
+
+"""
 function cross_correlation(f::AbstractArray{T}, g::AbstractArray{T}, dims=1) where {T<:Real}
     N = size(f, dims)
     @assert reduce(&, size(f) .== size(g)) "input sizes are not matched."
@@ -93,20 +103,20 @@ function cross_correlation(f::AbstractArray{T}, g::AbstractArray{T}, dims=1) whe
     G = fft_plan * g
 
     _xcorr = abs.(FFTW.irfft(F_bar .* G, N, dims))
-    FFTW.fftshift(_xcorr, dims)
+    return FFTW.fftshift(_xcorr, dims)
 end
 
 function cross_correlation_window(f::AbstractArray, dims=1; fs=1)
     N = size(f, dims)
     _t = FFTW.fftfreq(N, N / fs)
-    FFTW.fftshift(_t)
+    return FFTW.fftshift(_t)
 end
 
 function auto_correlation(f, dims=1; corrected=false)
     _corr = cross_correlation(f, f, dims)
     if corrected
         _corr = FFTW.ifftshift(_corr)
-        _corr[1] -= sum(_corr)
+        _corr[1] -= sum(_corr) #REIVEW: is this correct?
         FFTW.fftshift(_corr)
     else
         _corr
@@ -115,21 +125,25 @@ end
 
 @doc raw"""
 
+    covariogram(S1::AbstractMatrix{T}, S2::AbstractMatrix{T}; fs=1) where {T<:Real}
+
 The covariogram is defined as:
 
 ```math
 V \equiv \langle (S_1^{r} - P_1) \odot (S_2^{r} - P_2) \rangle = \langle S_1^{r} \odot S_2^{r} \rangle - P_1 \odot P_2
 ```
+
 """
+#TODO: documentation and reference
 function covariogram(S1::AbstractMatrix{T}, S2::AbstractMatrix{T}; fs=1) where {T<:Real}
     N = size(S1, 2)
-    P1 = mean(S1, dims=2)[:]
-    P2 = mean(S2, dims=2)[:]
-    σsq1 = var(S1, dims=2)[:]
-    σsq2 = var(S2, dims=2)[:]
+    P1 = mean(S1; dims=2)[:]
+    P2 = mean(S2; dims=2)[:]
+    σsq1 = var(S1; dims=2)[:]
+    σsq2 = var(S2; dims=2)[:]
 
     raw_xcorr_mat = cross_correlation(S1, S2, 1)
-    raw_xcorr = mean(raw_xcorr_mat, dims=2)[:]
+    raw_xcorr = mean(raw_xcorr_mat; dims=2)[:]
     shuffle_corr = cross_correlation(P1, P2)
     V = raw_xcorr .- shuffle_corr
     σsqV =
@@ -139,5 +153,5 @@ function covariogram(S1::AbstractMatrix{T}, S2::AbstractMatrix{T}; fs=1) where {
         ) ./ N
     σV = sqrt.(σsqV)
     trange = cross_correlation_window(S1, 1; fs)
-    (; t=trange, V, σ=σV)
+    return (; t=trange, V, σ=σV)
 end
